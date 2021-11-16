0960259ec30dc3567668beca6c53b076af0db1d8
FranciscoPugh <franciscopugh01@gmail.com>
2021-11-15 21:19:56 -0300

    Primer clase

---
+++ 1-Fundamentos-JavaScript/script.js
@@ -0,0 +1,37 @@
+/*var nombre = "Francis"
+var edad = 2.5
+var esProfesor = true //o false
+var nulo = null
+var indefinido = undefined
+
+nombre = 30
+var nombre = false
+
+let tutor = "Emiliano"
+tutor = "Federico"
+
+let gato = "Erich"
+gato = "Ceniza"
+
+const IVA = 1.21
+
+console.log(nombre)
+console.log(edad)
+console.log(esProfesor)
+console.log(tutor)
+//IVA = 1.30
+console.log(100 * IVA)
+console.log( 5 + 5)
+console.log("5" + "5")
+console.log(5 - "a" - 50000)
+console.log(5 + 5 + "5")*/
+
+let nombre = "Francisco"
+let apellido = "Pugh"
+
+let numero1 =  parseInt(prompt("Ingresa un numero"))
+let numero2 = parseFloat(prompt("Ingrese otro numero"))
+alert(numero1 + numero2)
+
+console.log(nombre + " " + apellido)
+console.log(`${nombre} ${apellido}`)
